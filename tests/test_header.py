08cf08d9c34ddf61820d577ae816de5494e358cf
Raph <raphael.garbarg@gmail.com>
2021-02-14 22:48:15 +0100

    init

---
+++ tests/test_header.py
@@ -0,0 +1,38 @@
+def test_has_magic():
+    assert False
+
+
+def test_write_magic():
+    assert False
+
+
+def test_get_nbits():
+    assert False
+
+
+def test_set_nbits():
+    assert False
+
+
+def test_get_is_compressed():
+    assert False
+
+
+def test_set_is_compressed():
+    assert False
+
+
+def test_get_is_encrypted():
+    assert False
+
+
+def test_set_is_encrypted():
+    assert False
+
+
+def test_get_data_length():
+    assert False
+
+
+def test_set_data_length():
+    assert False
---
+++ tests/test_image.py
@@ -0,0 +1,53 @@
+
+
+
+def test_set_data():
+    assert False
+
+
+def test_is_stegpy_image():
+    assert False
+
+
+def test_calculate_nbits():
+    assert False
+
+
+def test_is_compressed():
+    assert False
+
+
+def test_is_encrypted():
+    assert False
+
+
+def test_create_headers():
+    assert False
+
+
+def test_read_lsb():
+    assert False
+
+
+def test_write_lsb():
+    assert False
+
+
+def test_read_int():
+    assert False
+
+
+def test_write_int():
+    assert False
+
+
+def test_export():
+    assert False
+
+
+def test_create_payload():
+    assert False
+
+
+def test_extract_payload():
+    assert False
